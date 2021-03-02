6649660e231a37fa1feb6a39aaf46cbea7bba4e4
victornsgr <victornsgr@outlook.com>
2021-03-02 18:29:26 -0300

    Commit de estilos

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,15 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
-const webpack = require('webpack')
-environment.plugins.append(
-    'Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        Popper: ['popper.js', 'default']
-    })
-)
+
 
 
 module.exports = environment.toWebpackConfig()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default'],
+    Rails: '@rails/ujs'
+  })
+)
+
+
 module.exports = environment
